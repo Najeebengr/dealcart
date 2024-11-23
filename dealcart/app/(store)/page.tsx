f87d148ba709a2bd196cd418c2d7f69a8f4f2930
Najeeb <najeeb.nedo5555@gmail.com>
2024-11-23 12:15:50 +0500

    debugging

--- dealcart/app/(store)/page.tsx
+++ dealcart/app/(store)/page.tsx
@@ -8,6 +8,10 @@ export default async function Home() {
   // const products = await getAllProducts();
   const categories = await getAllCategories();
   // const subCategories = await getAllSubCategories();
+  console.log("Sanity Project ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+console.log("Sanity Dataset:", process.env.NEXT_PUBLIC_SANITY_DATASET);
+console.log("Sanity API Token:", process.env.SANITY_API_TOKEN);
+console.log("Sanity Read Token:", process.env.SANITY_API_READ_TOKEN);
   return (
 
     <section className="container mx-auto px-6 md:px-0">
